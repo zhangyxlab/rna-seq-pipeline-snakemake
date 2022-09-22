635fd5812e10a813aa20bacf4a65334982776f71
Shawn Zhang <zhangyanxiao@westlake.edu.cn>
2022-09-22 09:41:40 +0800

    update to zhang lab env

--- bin/run_rnaseq_vanilla.sh
+++ bin/run_rnaseq_vanilla.sh
@@ -7,8 +7,6 @@
 function usage(){
 echo -e "Usage: $0 -g genome -e E-mail -s server"
 echo -e "\t-g [genome]: hg19, mm10, etc."
-echo -e "\t-e [email]: email address."
-echo -e "\t-s [server]: silencer or TSCC"
 exit 1
 }
 
@@ -16,8 +14,6 @@ while getopts "g:e:s:" OPT
 do
   case $OPT in
     g) genome=$OPTARG;;
-    e) email=$OPTARG;;
-    s) server=$OPTARG;;
     \?)
       echo "Invalid option: -$OPTARG" >& 2
       usage
@@ -31,20 +27,17 @@ do
 done
 
 if [ $# -eq 0 ]; then usage; exit; fi
-if [ -z ${email+x} ];
-  then echo -e "Please provide E-mail"; usage; exit; fi
 if [ -z ${genome+x} ]; then
   echo -e "Please provide genome, eg. mm10, hg19"; usage;exit; fi
-if [ -z ${server+x} ];
-  then echo -e "Please tell us the server, eg. silencer, TSCC"; usage;exit; fi
 
+SERVER=$(hostname)
 NTHREADS=30
 DIR=$(dirname $0)
 LOG=run-$(date +%Y-%m-%d-%H-%M-%S).log
 . ${DIR}/validate_programs.sh
 
-if [ $server == "silencer" ]; then
-  source /projects/ps-renlab/share/Pipelines/environments/python3env/bin/activate
+if [ $SERVER == "heterochromatin.localdomain" ]; then
+  source /storage/zhangyanxiaoLab/share/Pipelines/environments/python3env/bin/activate
   ### unlock the directory
   touch Snakefile
   snakemake --unlock
@@ -54,10 +47,10 @@ if [ $server == "silencer" ]; then
   --configfile ${DIR}/config.yaml --config GENOME=$genome \
   2> >(tee -a $LOG >&2)
 
-elif [ $server == "TSCC" ]; then
+elif [ $SERVER == "login01.cluster.com" ]; then
 #  module load python
   unset PYTHONPATH
-  source /projects/ps-renlab/share/Pipelines/environments/python3env_TSCC/bin/activate
+  source /storage/zhangyanxiaoLab/share/Pipelines/environments/python3env_TSCC/bin/activate
   ### unlock the directory
   touch Snakefile
   snakemake --unlock
